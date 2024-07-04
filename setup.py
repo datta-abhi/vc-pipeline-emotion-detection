8bb9679f418552e3168d6d70b06b8f1173a72b12
datta-abhi <rookbee91@gmail.com>
2024-07-04 14:25:41 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Dummy Project to practice DVC data versioning and pipeline versioning through an emotion detection project',
+    author='abhigyan',
+    license='',
+)
